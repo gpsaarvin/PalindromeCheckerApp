c7c1367a798806c488fa77708317264d04de2fd0
G P Saarvin <gpsaarvin56@gmail.com>
2026-03-09 09:39:44 +0530

    Rename class to PalindromeCheckerApp

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 
 // UC6: Queue + Stack Based Palindrome Check
 
-public class UseCase6PalindromeCheckerApp {
+public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
